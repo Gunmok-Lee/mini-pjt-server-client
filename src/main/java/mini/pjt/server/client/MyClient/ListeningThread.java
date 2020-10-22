b022cc38580d81875cfe464e889f0aa985862362
antoniolee <antoniolee@192.168.0.33>
2020-10-22 12:38:12 +0900

    11

--- src/main/java/mini/pjt/server/client/MyClient/ListeningThread.java
+++ src/main/java/mini/pjt/server/client/MyClient/ListeningThread.java
@@ -4,11 +4,13 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import mini.pjt.server.client.MyClient.Command.CommandConfig;
+import mini.pjt.server.client.MyClient.Command.CommandReboot;
+import mini.pjt.server.client.MyClient.Command.CommandShutdown;
+import mini.pjt.server.client.MyClient.Command.CommandSystemOs;
 
 public class ListeningThread extends Thread { // �������� ���� �޼��� �д� Thread
   Socket socket = null;
-  String os = System.getProperty("os.name").toLowerCase();
-  String userOs;
 
   public ListeningThread(Socket socket) { // ������
     this.socket = socket; // �޾ƿ� Socket Parameter�� �ش� Ŭ���� Socket�� �ֱ�
@@ -21,55 +23,17 @@ public class ListeningThread extends Thread { // �������� ��
       InputStream input = socket.getInputStream(); // socket�� InputStream ������ InputStream in�� ���� ��
       BufferedReader reader = new BufferedReader(new InputStreamReader(input)); // BufferedReader�� �� InputStream�� ��� ���
       while(true) { // ���ѹݺ�
-        if (reader.readLine() == "/myOs") {
-          if (os.contains("mac")) {
-            userOs = "Mac";
-            System.out.printf("user os : %s\n", userOs);
-          }
-          if (os.contains("win")) {
-            userOs = "Windows";
-            System.out.printf("user os : %s\n", userOs);
-          } else if (os.contains("nix") || os.contains("nux") || os.contains("aix")) {
-            userOs = "Unix";
-            System.out.printf("user os : %s\n", userOs);
-          } else if (os.contains("linux")) {
-            userOs = "Linux";
-            System.out.printf("user os : %s\n", userOs);
-          }
-        }
-
-        if (reader.readLine() == "/config") {
-          if (os.contains("win")) {
-            try {
-              String line;
-              InputStream is;
-              is = Runtime.getRuntime().exec("ipconfig").getInputStream();
-              BufferedReader br = new BufferedReader(new InputStreamReader(is, "MS949"));
-              while((line = br.readLine()) != null) {
-                System.out.println(line);
-              }
-              br.close();
-              is.close();
-            } catch (Exception e) {
-              e.printStackTrace();
-            }
-          } else if (os.contains("nix") || os.contains("nux") || os.contains("aix") || os.contains("linux") || os.contains("mac")) {
-            try {
-              String line;
-              InputStream is;
-              is = Runtime.getRuntime().exec("ifconfig").getInputStream();
-              BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
-              while((line = br.readLine()) != null) {
-                System.out.println(line);
-              }
-              br.close();
-              is.close();
-            } catch (Exception e) {
-              e.printStackTrace();
-            }
-          }
-        }
-        System.out.println(reader.readLine());
+        String command = reader.readLine();
+        if (command.equals("/systemos")) {
+          new CommandSystemOs();
+        } else if (command.equals("/config")) {
+          new CommandConfig();
+        } else if (command.equals("/shutdown")) {
+          new CommandShutdown();
+        } else if (command.equals("/reboot")) {
+          new CommandReboot();
+        } else
+          System.out.println(command);
       }
 
 
@@ -77,7 +41,5 @@ public class ListeningThread extends Thread { // �������� ��
     } catch (Exception e) {
       e.printStackTrace();
     }
-
   }
-
 }
