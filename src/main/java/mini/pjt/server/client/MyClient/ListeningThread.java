772f1e07312b9972b97b6d261ea4d722a25b89dc
antoniolee <antoniolee@192.168.0.33>
2020-10-22 12:55:48 +0900

    Listening 수정

--- src/main/java/mini/pjt/server/client/MyClient/ListeningThread.java
+++ src/main/java/mini/pjt/server/client/MyClient/ListeningThread.java
@@ -4,10 +4,6 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.Socket;
-import mini.pjt.server.client.MyClient.Command.CommandConfig;
-import mini.pjt.server.client.MyClient.Command.CommandReboot;
-import mini.pjt.server.client.MyClient.Command.CommandShutdown;
-import mini.pjt.server.client.MyClient.Command.CommandSystemOs;
 
 public class ListeningThread extends Thread { // �������� ���� �޼��� �д� Thread
   Socket socket = null;
@@ -24,33 +20,141 @@ public class ListeningThread extends Thread { // �������� ��
       BufferedReader reader = new BufferedReader(new InputStreamReader(input)); // BufferedReader�� �� InputStream�� ��� ���
       while(true) { // ���ѹݺ�
         String command = reader.readLine();
+        String os = System.getProperty("os.name").toLowerCase();
         if (command.equals("/systemos")) {
-        	String os = System.getProperty("os.name").toLowerCase();
-            // os 구분자
-            String userOs;
-            // 클라이언트의 os표시
+          // os 구분자
+          String userOs;
+          // 클라이언트의 os표시
 
-            if (os.contains("mac")) {
-              userOs = "Mac";
-              System.out.printf("user os : %s\n", userOs);
-            } else if (os.contains("win")) {
-              userOs = "Windows";
-              System.out.printf("user os : %s\n", userOs);
-            } else if (os.contains("nix") || os.contains("nux") || os.contains("aix")) {
-              userOs = "Unix";
-              System.out.printf("user os : %s\n", userOs);
-            } else if (os.contains("linux")) {
-              userOs = "Linux";
-              System.out.printf("user os : %s\n", userOs);
-            }
+          if (os.contains("mac")) {
+            userOs = "Mac";
+            System.out.printf("user os : %s\n", userOs);
+          } else if (os.contains("win")) {
+            userOs = "Windows";
+            System.out.printf("user os : %s\n", userOs);
+          } else if (os.contains("nix") || os.contains("nux") || os.contains("aix")) {
+            userOs = "Unix";
+            System.out.printf("user os : %s\n", userOs);
+          } else if (os.contains("linux")) {
+            userOs = "Linux";
+            System.out.printf("user os : %s\n", userOs);
+          }
         } else if (command.equals("/config")) {
-          new CommandConfig();
+          if (os.contains("win")) {
+            try {
+              String line;
+              InputStream is;
+              is = Runtime.getRuntime().exec("ipconfig").getInputStream();
+              BufferedReader br = new BufferedReader(new InputStreamReader(is, "MS949"));
+              while((line = br.readLine()) != null) {
+                System.out.println(line); // ipconfig 명령 결과 출력
+              }
+              br.close();
+              is.close();
+            } catch (Exception e) {
+              e.printStackTrace();
+            }
+
+          } else if (os.contains("nix") || os.contains("nux") || os.contains("aix") || os.contains("linux") || os.contains("mac")) {
+            try {
+              String line;
+              InputStream is;
+              is = Runtime.getRuntime().exec("ifconfig").getInputStream();
+              BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+              while((line = br.readLine()) != null) {
+                System.out.println(line); // ifcongfig 명령 결과 출력
+              }
+              br.close();
+              is.close();
+            } catch (Exception e) {
+              e.printStackTrace();
+            }
+          }
         } else if (command.equals("/shutdown")) {
-          new CommandShutdown();
+          if (os.contains("linux")) {
+            try {
+              InputStream is;
+              System.out.println("OS를 강제 종료합니다.");
+              is = Runtime.getRuntime().exec("sudo shutdown -h now").getInputStream();
+              is.close();
+            } catch (Exception e) {
+              e.printStackTrace();
+            }
+
+          } else if (os.contains("win")) {
+            try {
+              InputStream is;
+              System.out.println("OS를 강제 종료합니다.");
+              is = Runtime.getRuntime().exec("shutdown -s -f").getInputStream();
+              is.close();
+            } catch (Exception e) {
+              e.printStackTrace();
+            }
+
+          } else if (os.contains("mac")) {
+            try {
+              InputStream is;
+              System.out.println("OS를 강제 종료합니다.");
+              is = Runtime.getRuntime().exec("sudo shutdown -h now").getInputStream();
+              is.close();
+            } catch (Exception e) {
+              e.printStackTrace();
+            }
+
+          } else if (os.contains("nix") || os.contains("nux") || os.contains("aix")) {
+            try {
+              InputStream is;
+              System.out.println("OS를 강제 종료합니다.");
+              is = Runtime.getRuntime().exec("shutdown -F").getInputStream();
+              is.close();
+            } catch (Exception e) {
+              e.printStackTrace();
+            }
+          }
         } else if (command.equals("/reboot")) {
-          new CommandReboot();
-        } else
+          if (os.contains("linux")) {
+            try {
+              InputStream is;
+              System.out.println("OS를 강제 리부팅 합니다.");
+              is = Runtime.getRuntime().exec("sudo shutdown -r now").getInputStream();
+              is.close();
+            } catch (Exception e) {
+              e.printStackTrace();
+            }
+
+          } else if (os.contains("win")) {
+            try {
+              InputStream is;
+              System.out.println("OS를 강제 리부팅 합니다.");
+              is = Runtime.getRuntime().exec("shutdown -r -t 0").getInputStream();
+              is.close();
+            } catch (Exception e) {
+              e.printStackTrace();
+            }
+
+          } else if (os.contains("mac")) {
+            try {
+              InputStream is;
+              System.out.println("OS를 강제 리부팅 합니다.");
+              is = Runtime.getRuntime().exec("sudo shutdown -r now").getInputStream();
+              is.close();
+            } catch (Exception e) {
+              e.printStackTrace();
+            }
+
+          } else if (os.contains("nix") || os.contains("nux") || os.contains("aix")) {
+            try {
+              InputStream is;
+              System.out.println("OS를 강제 리부팅 합니다.");
+              is = Runtime.getRuntime().exec("shutdown -Fr").getInputStream();
+              is.close();
+            } catch (Exception e) {
+              e.printStackTrace();
+            }
+          }
+        } else {
           System.out.println(command);
+        }
       }
 
 
